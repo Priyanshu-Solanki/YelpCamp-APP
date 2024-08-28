3423b2a8868fedbb126e5bafc5b65a335172762a
Priyanshu-Solanki <priyanshsolanki2015@gmail.com>
2024-08-29 02:42:09 +0530

    Errors & Validating Data

---
+++ YelpCamp/utilities/ExpressErorrs.js
@@ -0,0 +1,10 @@
+class ExpressErrors extends Error{
+    constructor(message, status)
+    {
+        super()
+        this.message = message
+        this.status = status
+    }
+}
+
+module.exports = ExpressErrors
